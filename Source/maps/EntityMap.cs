03b5290238375db30d1faef2df7eb067d6fecf4f
MysteryCallerA <mysterycallera@gmail.com>
2023-04-11 22:05:36 -0700

    Various changes

--- Source/maps/EntityMap.cs
+++ Source/maps/EntityMap.cs
@@ -10,60 +10,35 @@ using Utils.data;
 using Utils.save;
 
 namespace Toybox.maps {
-	public class EntityMap:IXmlSaveable {
+	public class EntityMap<T>:IXmlSaveable where T:Entity {
 
 		private Dictionary<Point, LinkedList<int>> Map = new Dictionary<Point, LinkedList<int>>();
-		private StableList<Entity> Entities = new StableList<Entity>();
+		private StableList<T> Entities = new StableList<T>();
 
 		private bool Updating = false;
-		private HashSet<Entity> WillAdd = new HashSet<Entity>();
-		private HashSet<Entity> WillRemove = new HashSet<Entity>();
+		private HashSet<T> WillAdd = new HashSet<T>();
+		private HashSet<T> WillRemove = new HashSet<T>();
 
 		private int CellWidth;
 		private int CellHeight;
 
-		private List<Entity> VisibleEntities = new List<Entity>();
+		private List<T> VisibleEntities = new List<T>();
 
 		public EntityMap(int cellwidth, int cellheight) {
 			CellWidth = cellwidth;
 			CellHeight = cellheight;
 		}
 
-		public void Draw(SpriteBatch s, Camera c) {
-			var cells = GetVisibleCells(c.GetWorldBounds());
-
-			VisibleEntities.Clear();
-			foreach (var cell in cells) {
-				foreach (var id in Map[cell]) {
-					VisibleEntities.Add(Entities[id]);
-				}
-			}
+		public void Draw(Renderer r, Camera c) {
+			VisibleEntities = GetEntitiesNearBounds(c.GetWorldBounds());//Possible bug: this might need to be c.GetGameBounds?
 			VisibleEntities.Sort();
 
 			foreach (var e in VisibleEntities) {
-				e.Draw(s, c);
+				e.Draw(r, c);
 			}
 		}
 
-		private List<Point> GetVisibleCells(Rectangle bounds) {
-			var topleft = GetCell(bounds.X, bounds.Y);
-			topleft.X -= 1;
-			topleft.Y -= 1;
-			var botright = GetCell(bounds.Right, bounds.Bottom);
-
-			var output = new List<Point>();
-			for (int x = topleft.X; x <= botright.X; x++) {
-				for (int y = topleft.Y; y <= botright.Y; y++) {
-					var p = new Point(x, y);
-					if (Map.ContainsKey(p)) {
-						output.Add(p);
-					}
-				}
-			}
-			return output;
-		}
-
-		public void Add(Entity e) {
+		public void Add(T e) {
 			if (Updating) {
 				WillAdd.Add(e);
 				return;
@@ -74,7 +49,7 @@ namespace Toybox.maps {
 			AddToMap(e);
 		}
 
-		public void Add(ICollection<Entity> ents) {
+		public void Add(ICollection<T> ents) {
 			foreach (var e in ents) {
 				Add(e);
 			}
@@ -88,7 +63,7 @@ namespace Toybox.maps {
 			return null;
 		}
 
-		public void Remove(Entity e) {
+		public void Remove(T e) {
 			if (Updating) {
 				WillRemove.Add(e);
 				return;
@@ -101,25 +76,25 @@ namespace Toybox.maps {
 
 		public void Update() {
 			Updating = true;
-			foreach (Entity e in Entities) {
+			foreach (T e in Entities) {
 				if (e == null) continue;
 				e.Update();
 				UpdateMapPosition(e);
 			}
 			Updating = false;
 
-			foreach (Entity e in WillAdd) {
+			foreach (T e in WillAdd) {
 				Add(e);
 			}
 			WillAdd.Clear();
 
-			foreach (Entity e in WillRemove) {
+			foreach (T e in WillRemove) {
 				Remove(e);
 			}
 			WillRemove.Clear();
 		}
 
-		private void AddToMap(Entity e) {
+		private void AddToMap(T e) {
 			var cell = GetCell(e.X, e.Y);
 			if (!Map.ContainsKey(cell)) {
 				Map.Add(cell, new LinkedList<int>());
@@ -128,7 +103,7 @@ namespace Toybox.maps {
 			e.MapCell = cell;
 		}
 
-		private void RemoveFromMap(Entity e) {
+		private void RemoveFromMap(T e) {
 			if (Map[e.MapCell].Count == 1) {
 				Map.Remove(e.MapCell);
 				return;
@@ -136,7 +111,7 @@ namespace Toybox.maps {
 			Map[e.MapCell].Remove(e.Id);
 		}
 
-		private void UpdateMapPosition(Entity e) {
+		private void UpdateMapPosition(T e) {
 			var cell = GetCell(e.X, e.Y);
 			if (cell == e.MapCell) return;
 
@@ -164,8 +139,8 @@ namespace Toybox.maps {
 			return new Point((int)Math.Floor((float)x / CellWidth), (int)Math.Floor((float)y / CellHeight));
 		}
 
-		public Entity GetEntity(Point gamepos) {
-			var possible = new List<Entity>();
+		public T GetEntity(Point gamepos) {
+			var possible = new List<T>();
 			var cell = GetCell(gamepos.X, gamepos.Y);
 			AddEntitiesFromCell(possible, cell);
 			cell.X -= 1;
@@ -182,11 +157,11 @@ namespace Toybox.maps {
 			return null;
 		}
 
-		public Entity GetEntity(int id) {
+		public T GetEntity(int id) {
 			return Entities[id];
 		}
 
-		public bool TryGetEntity(int id, out Entity e) {
+		public bool TryGetEntity(int id, out T e) {
 			if (!Entities.IsIdValid(id)) {
 				e = null;
 				return false;
@@ -195,7 +170,7 @@ namespace Toybox.maps {
 			return e != null;
 		}
 
-		private void AddEntitiesFromCell(List<Entity> output, Point cell) {
+		private void AddEntitiesFromCell(List<T> output, Point cell) {
 			if (!Map.ContainsKey(cell)) return;
 			foreach (var id in Map[cell]) {
 				output.Add(Entities[id]);
@@ -204,7 +179,7 @@ namespace Toybox.maps {
 
 		/// <summary> Checks if the size of cells is large enough for the contained entities. Cells should be at least as large as the largest entity. </summary>
 		public bool ValidateCellSize() {
-			foreach (Entity e in Entities) {
+			foreach (T e in Entities) {
 				if (e == null) continue;
 				var box = e.GetHitbox();
 				if (box.Width > CellWidth || box.Height > CellHeight) return false;
@@ -213,6 +188,47 @@ namespace Toybox.maps {
 		}
 
 		public void Save(XmlWriter writer) {
+		}
+
+		private List<Point> GetCellsNearBounds(Rectangle bounds) {
+			var topleft = GetCell(bounds.X, bounds.Y);
+			topleft.X -= 1;
+			topleft.Y -= 1;
+			var botright = GetCell(bounds.Right, bounds.Bottom);
+
+			var output = new List<Point>();
+			for (int x = topleft.X; x <= botright.X; x++) {
+				for (int y = topleft.Y; y <= botright.Y; y++) {
+					var p = new Point(x, y);
+					if (Map.ContainsKey(p)) {
+						output.Add(p);
+					}
+				}
+			}
+			return output;
+		}
+
+		public List<T> GetEntitiesNearBounds(Rectangle bounds) {
+			var cells = GetCellsNearBounds(bounds);
+			var output = new List<T>();
+			foreach (var cell in cells) {
+				foreach (var id in Map[cell]) {
+					output.Add(Entities[id]);
+				}
+			}
+			return output;
+		}
+
+		public IEnumerable<T> GetCollisions(Rectangle collider) {
+			var cells = GetCellsNearBounds(collider);
+			foreach (var cell in cells) {
+				foreach (var id in Map[cell]) {
+					var e = Entities[id];
+					if (e.GetHitbox().Intersects(collider)) {
+						yield return e;
+					}
+				}
+			}
 		}
 	}
 }
