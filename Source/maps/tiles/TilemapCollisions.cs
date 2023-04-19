ae45d82b31f3325ce34aa3e49c682d6aed5b4d04
MysteryCallerA <mysterycallera@gmail.com>
2023-04-19 01:26:16 -0700

    Create TilemapCollisions.cs

---
+++ Source/maps/tiles/TilemapCollisions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toybox.maps.tiles {
+	
+	public static class TilemapCollisions {
+
+		public class TileData {
+			public Rectangle Bounds;
+			public Tilemap.Tile Tile;
+		}
+
+		public static IEnumerable<TileData> GetCollisions(this Tilemap t, Rectangle r) {
+			var topleft = t.PixelToMap(r.Left, r.Top);
+			var botright = t.PixelToMap(r.Right, r.Bottom);
+
+			for (int x = topleft.X; x <= botright.X; x++) {
+				for (int y = topleft.Y; y <= botright.Y; y++) {
+					var tile = t.Get(x, y);
+					if (tile.HasValue) {
+						yield return new TileData() { Tile = tile.Value, Bounds = new Rectangle(t.MapToPixel(x, y), t.TileSize) };
+					}
+				}
+			}
+		}
+	
+	}
+}
