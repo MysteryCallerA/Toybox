8a1a2ed49a35b0cfbca887889174b1ad3d5d92f8
MysteryCallerA <mysterycallera@gmail.com>
2023-05-06 03:06:15 -0700

    Create AttackBox.cs

---
+++ Source/components/action/AttackBox.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.input;
+
+namespace Toybox.components.action {
+	public class AttackBox:EntityComponent {
+
+		public VirtualKey AttackButton;
+		public Action<AttackBox> AttackTransform;
+		public string AnchorName = "front";
+
+		public Point StartSize = new Point(5, 5);
+		public Point Size;
+		public Color StartColor = Color.Red;
+		public Color Color;
+		public Point StartOffset = new Point(0, 5);
+		public Point Offset;
+		public int Time = 20;
+
+		public PointRay Anchor;
+		public int Timer = 0;
+
+		public AttackBox(VirtualKey attack) {
+			AttackButton = attack;
+		}
+
+		public void Apply(ComplexEntity e) {
+			if (!Attacking && AttackButton.Pressed) {
+				Attack();
+			}
+
+			if (Attacking) {
+				Anchor = e.Anchors[AnchorName];
+				AttackTransform?.Invoke(this);
+				Timer++;
+				if (Timer >= Time) EndAttack();
+			}
+		}
+
+		public virtual void Attack() {
+			if (Attacking) return;
+			Attacking = true;
+			Timer = 0;
+			Size = StartSize;
+			Color = StartColor;
+			Offset = StartOffset;
+		}
+
+		protected virtual void EndAttack() {
+			Attacking = false;
+		}
+
+		public void Draw(Entity e, Renderer r, Camera c) {
+			if (!Attacking) return;
+
+			float opacity = (float)Timer / Time;
+			r.DrawRect(Bounds, Color * opacity, c, Camera.Space.Subpixel);
+		}
+
+		public bool Attacking {
+			get; private set;
+		} = false;
+
+		public Point Position {
+			get {
+				if (Anchor.Xdir < 0) {
+					return new Point(Anchor.Start.X - Size.X - Offset.X, Anchor.Start.Y + Offset.Y);
+				}
+				return Anchor.Start + Offset;
+			}
+		}
+
+		public Rectangle Bounds {
+			get { return new Rectangle(Position, Size); }
+		}
+	}
+}
