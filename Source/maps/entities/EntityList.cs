587cea0172474fa3c9ea8f2f0776f8c1c8e98b43
MysteryCallerA <mysterycallera@gmail.com>
2023-05-31 00:28:11 -0700

    Update EntityList.cs

--- Source/maps/entities/EntityList.cs
+++ Source/maps/entities/EntityList.cs
@@ -1,60 +1,77 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Toybox.maps.entities
-{
-    public class EntityList
-    {
+namespace Toybox.maps.entities {
 
-        private List<Entity> Content = new List<Entity>();
+	public class EntityList:EntityList<Entity> {
+	}
 
-        public EntityList()
-        {
+	public class EntityList<T>:IEnumerable<T> where T : Entity {
 
-        }
+		private List<T> Content = new List<T>();
 
-        public void Update()
-        {
-            foreach (var e in Content)
-            {
-                e.Update();
-            }
-        }
+		public EntityList() {
 
-        public void Draw(Renderer r, Camera c)
-        {
-            foreach (var e in Content)
-            {
-                e.Draw(r, c);
-            }
-        }
+		}
 
-        public void Add(Entity e)
-        {
-            Content.Add(e);
-        }
+		public T this[int i] {
+			get { return Content[i]; }
+			set { Content[i] = value; }
+		}
 
-        public void Add(ICollection<Entity> ents)
-        {
-            foreach (var e in ents)
-            {
-                Add(e);
-            }
-        }
+		public int Count {
+			get { return Content.Count; }
+		}
 
-        public Entity Find(string name)
-        {
-            foreach (Entity e in Content)
-            {
-                if (e == null) continue;
-                if (e.Name == name) return e;
-            }
-            return null;
-        }
+		public void Update() {
+			foreach (var e in Content) {
+				e.Update();
+			}
+		}
 
-    }
+		public void Draw(Renderer r, Camera c) {
+			foreach (var e in Content) {
+				e.Draw(r, c);
+			}
+		}
+
+		public void Add(T e) {
+			Content.Add(e);
+		}
+
+		public void Add(ICollection<T> ents) {
+			foreach (var e in ents) {
+				Add(e);
+			}
+		}
+
+		public T Find(string name) {
+			foreach (T e in Content) {
+				if (e == null) continue;
+				if (e.Name == name) return e;
+			}
+			return null;
+		}
+
+		public T Find(Point p) {
+			foreach (T e in Content) {
+				if (e.GetHitbox().Contains(p)) return e;
+			}
+			return null;
+		}
+
+		public IEnumerator<T> GetEnumerator() {
+			return Content.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+	}
 }
