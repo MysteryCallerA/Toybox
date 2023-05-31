1267812edd5cf6720c2bc665e8ebc4b212c7cacd
MysteryCallerA <mysterycallera@gmail.com>
2023-05-31 00:28:34 -0700

    Create EntityFactory.cs

---
+++ Source/EntityFactory.cs
@@ -0,0 +1,44 @@
+﻿using MonoGame.Framework.Utilities.Deflate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toybox.tiled;
+
+namespace Toybox {
+
+	public abstract class EntityFactory<T> where T:Entity {
+
+		protected abstract T CreateEntity(string type);
+
+		public virtual void PositionEntity(Entity e, TiledObject o) {
+			e.Position = Resources.Camera.Project(Camera.Space.Pixel, Camera.Space.Subpixel, o.Position); //This projection should probably be configurable somehow
+			var hitbox = e.GetHitbox();
+			e.X -= hitbox.Width / 2;
+			e.Y -= hitbox.Bottom - e.Y;
+		}
+
+		protected virtual void InitializeEntity(T e, TiledObject o) {
+		}
+
+		public T BuildEntity(TiledObject o) {
+			var e = CreateEntity(o.Name);
+			if (e == null) return null;
+			PositionEntity(e, o);
+			InitializeEntity(e, o);
+			return e;
+		}
+
+		public List<T> BuildAll(List<TiledObject> objects) {
+			var output = new List<T>();
+			foreach (var o in objects) {
+				var entity = BuildEntity(o);
+				if (entity == null) continue;
+				output.Add(entity);
+			}
+			return output;
+		}
+	
+	}
+}
