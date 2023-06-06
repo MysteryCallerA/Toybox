dd39a15120b4f6c471ef936c7481325f3097b83b
MysteryCallerA <mysterycallera@gmail.com>
2023-06-06 03:20:22 -0700

    Create Attributes.cs

---
+++ Source/Attributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toybox {
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public class DebugReadAttribute:Attribute {
+	}
+
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public class DebugWriteAttribute:Attribute {
+	}
+}
