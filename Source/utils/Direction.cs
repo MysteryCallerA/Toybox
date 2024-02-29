5c43cba8631829c0d3121b731210079c04c3f96f
MysteryCallerA <mysterycallera@gmail.com>
2024-02-29 00:35:14 -0800

    Create Direction.cs

---
+++ Source/utils/Direction.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toybox.utils {
+
+	public enum Direction {
+		Neutral, Up, UpRight, Right, DownRight, Down, DownLeft, Left, UpLeft
+	}
+
+	public static class DirectionOps {
+
+		public static Direction Vertical(this Direction d) {
+			if (d == Direction.UpLeft || d == Direction.Up || d == Direction.UpRight) return Direction.Up;
+			if (d == Direction.DownLeft || d == Direction.Down || d == Direction.DownRight) return Direction.Down;
+			return Direction.Neutral;
+		}
+
+		public static Direction Horizontal(this Direction d) {
+			if (d == Direction.UpLeft || d == Direction.Left || d == Direction.DownLeft) return Direction.Left;
+			if (d == Direction.UpRight || d == Direction.Right || d == Direction.DownRight) return Direction.Right;
+			return Direction.Neutral;
+		}
+
+		public static Direction SwitchedUp(this Direction d) {
+			if (d == Direction.UpLeft || d == Direction.Up || d == Direction.UpRight) return d;
+			if (d == Direction.Left || d == Direction.DownLeft) return Direction.UpLeft;
+			if (d == Direction.Right || d == Direction.DownRight) return Direction.UpRight;
+			return Direction.Up;
+		}
+
+		public static Direction SwitchedDown(this Direction d) {
+			if (d == Direction.DownLeft || d == Direction.Down || d == Direction.DownRight) return d;
+			if (d == Direction.UpLeft || d == Direction.Left) return Direction.DownLeft;
+			if (d == Direction.UpRight || d == Direction.Right) return Direction.DownRight;
+			return Direction.Down;
+		}
+
+		public static Direction SwitchedLeft(this Direction d) {
+			if (d == Direction.UpLeft || d == Direction.Left || d == Direction.DownLeft) return d;
+			if (d == Direction.UpRight || d == Direction.Up) return Direction.UpLeft;
+			if (d == Direction.DownRight || d == Direction.Down) return Direction.DownLeft;
+			return Direction.Left;
+		}
+
+		public static Direction SwitchedRight(this Direction d) {
+			if (d == Direction.UpRight || d == Direction.Right || d == Direction.DownRight) return d;
+			if (d == Direction.UpLeft || d == Direction.Up) return Direction.UpRight;
+			if (d == Direction.DownLeft || d == Direction.Down) return Direction.DownRight;
+			return Direction.Right;
+		}
+
+	}
+
+}
