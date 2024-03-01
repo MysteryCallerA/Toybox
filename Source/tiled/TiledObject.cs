65cad0b6fdc3d9dc6bebf1076840219b491561a6
MysteryCallerA <mysterycallera@gmail.com>
2024-03-01 03:15:49 -0800

    added TiledObject.Size

--- Source/tiled/TiledObject.cs
+++ Source/tiled/TiledObject.cs
@@ -11,10 +11,14 @@ namespace Toybox.tiled {
 
 		public string Name;
 		public Point Position;
+		public Point Size;
+
+		public Rectangle Bounds { get { return new Rectangle(Position, Size); } }
 
 		public TiledObject(XmlNode data) {
 			Name = data.Attributes["name"].Value;
 			Position = new Point(int.Parse(data.Attributes["x"].Value), int.Parse(data.Attributes["y"].Value));
+			Size = new Point(int.Parse(data.Attributes["width"].Value), int.Parse(data.Attributes["height"].Value));
 		}
 
 	}
