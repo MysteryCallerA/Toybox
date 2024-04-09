c9bb7c57198a45ccab0b7e8e5502e55a47623369
MysteryCallerA <mysterycallera@gmail.com>
2024-04-08 22:45:42 -0700

    started chunk system

---
+++ Source/scenes/world/WorldManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toybox.scenes.world {
+	public class WorldManager<T> where T:WorldSegment {
+
+		public List<T> Content = new();
+		public WorldLayout Layout;
+
+		public WorldManager() {
+
+		}
+
+		public void Draw(Renderer r, Camera c) {
+			for (int i = 0; i < Content.Count; i++) {
+				Content[i].Draw(r, c);
+			}
+		}
+
+		public void Update() {
+			for (int i = 0; i < Content.Count; i++) {
+				Content[i].Update();
+			}
+		}
+
+	}
+}
---
+++ Source/scenes/world/WorldSegment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toybox.scenes.world {
+	public abstract class WorldSegment {
+
+		public string Name;
+
+		public WorldSegment(string name) {
+			Name = name;
+		}
+
+		public abstract void Draw(Renderer r, Camera c);
+		public abstract void Update();
+
+	}
+}
