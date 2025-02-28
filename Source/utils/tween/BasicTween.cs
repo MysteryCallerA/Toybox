19bfb04d3c65244465f10a0aa23033d08a123228
MysteryCallerA <mysterycallera@gmail.com>
2025-02-28 02:24:16 -0800

    Create BasicTween.cs

---
+++ Source/utils/tween/BasicTween.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toybox.utils.tween {
+	public class BasicTween:Tween {
+
+		public Func<float, float> Function;
+
+		public BasicTween(Func<float, float> easingFunction, int frames) {
+			Frames = frames;
+			Function = easingFunction;
+		}
+
+		public override float Get(int frame) {
+			if (frame < 0) frame = 0;
+			else if (frame > Frames) frame = Frames;
+			return Function.Invoke((float)frame / Frames);
+		}
+	}
+}
