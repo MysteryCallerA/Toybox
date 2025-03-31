e8b07db725ce68ebc97e96c5fa30b0e18fede1ee
MysteryCallerA <mysterycallera@gmail.com>
2025-03-31 05:41:20 -0700

    added scrollbar

---
+++ Source/gui/graphic/VScrollbarGraphic.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toybox.gui.core;
+using Toybox.gui.layout;
+
+namespace Toybox.gui.graphic {
+	public class VScrollbarGraphic:MenuElement {
+
+		public MenuElement FrontGraphic;
+		public MenuElement BackGraphic;
+		private readonly MenuVScrollLayout Source;
+
+		private bool ScrollbarNeeded;
+
+		public VScrollbarGraphic(MenuVScrollLayout parent) {
+			BackGraphic = new MenuBoxGraphic() { BackColor = Color.DarkGray };
+			FrontGraphic = new MenuBoxGraphic() { BackColor = Color.White };
+			Source = parent;
+			Setup();
+		}
+
+		public VScrollbarGraphic(MenuVScrollLayout parent, MenuElement back, MenuElement front) {
+			Source = parent;
+			BackGraphic = back;
+			FrontGraphic = front;
+			Setup();
+		}
+
+		private void Setup() {
+			HFit = FitType.Static;
+			VFit = FitType.FillOuter;
+			TargetInnerSize = new Point(5, 0);
+			MarginLeft = 1;
+			HAlign = HAlignType.Right;
+		}
+
+		protected override void UpdateContentSize(Point contentContainerSize, out Point contentSize) {
+			BackGraphic.UpdateSize(contentContainerSize);
+
+			var innerRatio = (float)Source.VisibleVertical / Source.TotalVertical;
+			if (innerRatio >= 1) {
+				innerRatio = 1;
+				ScrollbarNeeded = false;
+			} else {
+				ScrollbarNeeded = true;
+			}
+			FrontGraphic.UpdateSize(new Point(contentContainerSize.X, (int)(innerRatio * contentContainerSize.Y)));
+			contentSize = Point.Zero;
+		}
+
+		protected internal override void UpdateContentPositions() {
+			BackGraphic.Position = ContentOrigin;
+
+			var posRatio = (float)Source.Scroll / Source.TotalVertical;
+			FrontGraphic.Position = new Point(BackGraphic.Position.X, (int)(posRatio * Source.VisibleVertical) + BackGraphic.Position.Y);
+
+			BackGraphic.UpdateContentPositions();
+			FrontGraphic.UpdateContentPositions();
+		}
+
+		public override void Draw(Renderer r) {
+			if (!ScrollbarNeeded) return;
+
+			BackGraphic.Draw(r);
+			FrontGraphic.Draw(r);
+		}
+	}
+}
--- Source/gui/layout/MenuVScrollLayout.cs
+++ Source/gui/layout/MenuVScrollLayout.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Toybox.gui.core;
+using Toybox.gui.graphic;
 using Toybox.utils.tween;
 
 namespace Toybox.gui.layout {
@@ -18,6 +19,14 @@ namespace Toybox.gui.layout {
 		private int TweenEnd;
 		private int TweenFrame;
 
+		public VScrollbarGraphic Scrollbar;
+		public int VisibleVertical { get; private set; }
+		public int TotalVertical { get; private set; }
+
+		public MenuVScrollLayout() {
+			Scrollbar = new VScrollbarGraphic(this);
+		}
+
 		public override void UpdateContentSize(Point contentContainerSize, out Point contentSize) {
 			if (Parent.Content.Count == 0) {
 				contentSize = Point.Zero;
@@ -26,14 +35,24 @@ namespace Toybox.gui.layout {
 
 			var bounds = new Point(contentContainerSize.X, 3);
 			int hsize = 0;
+			int vsize = Spacing * (Parent.Content.Count - 1);
 
 			foreach (var e in Parent.Content) {
 				e.UpdateSize(bounds);
 				var esize = e.OuterSize;
 				if (esize.X > hsize) hsize = esize.X;
+				vsize += esize.Y;
 			}
 
 			contentSize = new Point(hsize, contentContainerSize.Y);
+
+			if (Scrollbar != null) {
+				VisibleVertical = contentSize.Y;
+				TotalVertical = vsize;
+				int scrollWidth = contentSize.X + Parent.MarginLeft + Parent.MarginRight + Parent.PaddingLeft + Parent.PaddingRight;
+				int scrollHeight = contentSize.Y + Parent.MarginTop + Parent.MarginBottom + Parent.PaddingTop + Parent.PaddingBottom;
+				Scrollbar.UpdateSize(new Point(scrollWidth, scrollHeight));
+			}
 		}
 
 		public override void UpdateContentPosition() {
@@ -53,9 +72,21 @@ namespace Toybox.gui.layout {
 				y += e.OuterSize.Y + Spacing;
 				e.UpdateContentPositions();
 			}
+
+			if (Scrollbar != null) {
+				var totalBounds = Parent.OuterBounds;
+				if (Scrollbar.HAlign == MenuElement.HAlignType.Left) {
+					Scrollbar.Position = new Point(totalBounds.Left - Scrollbar.OuterSize.X, totalBounds.Y);
+				} else {
+					Scrollbar.Position = new Point(totalBounds.Right, totalBounds.Y);
+				}
+				Scrollbar.UpdateContentPositions();
+			}
 		}
 
 		public override void DrawContent(Renderer r) {
+			Scrollbar?.Draw(r);
+
 			r.End();
 			r.Begin(Parent.ContentBounds);
 			base.DrawContent(r);
