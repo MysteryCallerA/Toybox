94c06fd38313af4cdcffa8637943f9d643bdf95a
MysteryCallerA <mysterycallera@gmail.com>
2025-04-01 01:39:21 -0700

    Create MenuDynamicText.cs

---
+++ Source/gui/content/MenuDynamicText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toybox.utils.text;
+
+namespace Toybox.gui.content {
+
+	public class MenuDynamicText:MenuText {
+
+		public Func<string> GetText;
+
+		public MenuDynamicText(Font f):base(f) {
+		}
+
+		public MenuDynamicText(Font f, Func<string> getText):base(f) {
+			GetText = getText;
+		}
+
+		protected internal override void UpdateState() {
+			base.UpdateState();
+
+			if (GetText != null) {
+				Content = GetText.Invoke();
+			}
+		}
+
+	}
+}
